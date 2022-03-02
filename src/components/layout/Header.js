292d29bb63280d87e8bc836c77e12b0625fb3dda
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-03-02 09:13:05 -0600

    Creating new components

---
+++ src/components/layout/Header.js
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header className='line'>
+      <div className='container'>
+        <h1>CRM - Client Administrator</h1>
+      </div>
+    </header>
+  )
+}
