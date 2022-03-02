892458e8e1ba73b3f87a66791f7a09287157b821
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-03-02 15:41:04 -0600

    Add form to new customers

--- src/components/customers/NewCustomer.js
+++ src/components/customers/NewCustomer.js
@@ -1,3 +1,39 @@
 export const NewCustomer = () => {
-  return <div>NewCustomer</div>
+  return (
+    <>
+      <h2>New Customer</h2>
+      <form>
+        <legend>Fill in all the fields</legend>
+
+        <div className='campo'>
+          <label>Name:</label>
+          <input type='text' placeholder='Name Customer' name='name' />
+        </div>
+
+        <div className='campo'>
+          <label>LastName:</label>
+          <input type='text' placeholder='LastName Customer' name='lastName' />
+        </div>
+
+        <div className='campo'>
+          <label>Company:</label>
+          <input type='text' placeholder='Company Customer' name='company' />
+        </div>
+
+        <div className='campo'>
+          <label>Email:</label>
+          <input type='email' placeholder='Email Customer' name='email' />
+        </div>
+
+        <div className='campo'>
+          <label>Phone:</label>
+          <input type='email' placeholder='Phone Customer' name='phone' />
+        </div>
+
+        <div className='send'>
+          <input type='submit' className='btn btn-blue' value='Add Customer' />
+        </div>
+      </form>
+    </>
+  )
 }
