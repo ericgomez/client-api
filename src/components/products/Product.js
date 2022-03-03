d3532986e1e1168804ca3a822a32469d04ccdcb3
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-03-03 11:29:27 -0600

    Listing the products and images

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -1,3 +1,34 @@
-export const Product = () => {
-  return <div>Product</div>
+import { Link } from 'react-router-dom'
+
+export const Product = ({ product }) => {
+  const { _id, name, price, image } = product
+
+  const handleDeleted = async id => {
+    console.log(id)
+  }
+
+  return (
+    <li className='product'>
+      <div className='info-product'>
+        <p className='name'>{name}</p>
+        <p className='price'>${price} </p>
+        {image && <img src={`http://localhost:5000/${image}`} alt={image} />}
+      </div>
+      <div className='actions'>
+        <Link to={`/products/edit/${_id}`} className='btn btn-blue'>
+          <i className='fas fa-pen-alt'></i>
+          Edit Product
+        </Link>
+
+        <button
+          type='button'
+          className='btn btn-red btn-delete'
+          onClick={() => handleDeleted(_id)}
+        >
+          <i className='fas fa-times'></i>
+          Delete Product
+        </button>
+      </div>
+    </li>
+  )
 }
--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -2,13 +2,14 @@ import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 import instanceAxios from './../../config/axios'
+import { Product } from './../../components/products/Product'
 
 export const Products = () => {
   const [products, setProducts] = useState([])
 
   const getProductsAPI = async () => {
     const response = await instanceAxios.get('/products')
-    setProducts(response.data.products)
+    setProducts(response.data)
   }
 
   useEffect(() => {
@@ -25,60 +26,9 @@ export const Products = () => {
       </Link>
 
       <ul className='list-products'>
-        <li className='product'>
-          <div className='info-product'>
-            <p className='name'>VueJS</p>
-            <p className='price'>$25.00 </p>
-            <img src='img/1.jpg' alt='' />
-          </div>
-          <div className='actions'>
-            <Link to={`/products/`} className='btn btn-blue'>
-              <i className='fas fa-pen-alt'></i>
-              Edit Product
-            </Link>
-
-            <button type='button' className='btn btn-red btn-delete'>
-              <i className='fas fa-times'></i>
-              Delete Product
-            </button>
-          </div>
-        </li>
-        <li className='product'>
-          <div className='info-product'>
-            <p className='name'>AngularJS</p>
-            <p className='price'>$25.00 </p>
-            <img src='img/2.jpg' alt='' />
-          </div>
-          <div className='actions'>
-            <Link to={`/products/`} className='btn btn-blue'>
-              <i className='fas fa-pen-alt'></i>
-              Edit Product
-            </Link>
-
-            <button type='button' className='btn btn-red btn-delete'>
-              <i className='fas fa-times'></i>
-              Delete Product
-            </button>
-          </div>
-        </li>
-        <li className='product'>
-          <div className='info-product'>
-            <p className='name'>ReactJS</p>
-            <p className='price'>$25.00 </p>
-            <img src='img/3.jpg' alt='' />
-          </div>
-          <div className='actions'>
-            <Link to={`/products/`} className='btn btn-blue'>
-              <i className='fas fa-pen-alt'></i>
-              Edit Product
-            </Link>
-
-            <button type='button' className='btn btn-red btn-delete'>
-              <i className='fas fa-times'></i>
-              Delete Product
-            </button>
-          </div>
-        </li>
+        {products.map(product => (
+          <Product key={product._id} product={product} />
+        ))}
       </ul>
     </>
   )
