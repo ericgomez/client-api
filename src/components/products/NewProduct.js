0535a5d7b90bd5230a11a548b4f0a412b06d7196
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-03-03 16:39:05 -0600

    Creating form to add new products

--- src/components/products/NewProduct.js
+++ src/components/products/NewProduct.js
@@ -1,3 +1,44 @@
+import { useState } from 'react'
+
 export const NewProduct = () => {
-  return <div>NewProduct</div>
+  const [product, setProduct] = useState({
+    name: '',
+    price: '',
+    image: ''
+  })
+
+  return (
+    <>
+      <h2>New Product</h2>
+
+      <form>
+        <legend>Fill in all the fields</legend>
+
+        <div className='campo'>
+          <label>Name:</label>
+          <input type='text' placeholder='Product Name' name='name' />
+        </div>
+
+        <div className='campo'>
+          <label>Price:</label>
+          <input
+            type='number'
+            name='price'
+            min='0.00'
+            step='0.01'
+            placeholder='Price'
+          />
+        </div>
+
+        <div className='campo'>
+          <label>Image:</label>
+          <input type='file' name='image' />
+        </div>
+
+        <div className='send'>
+          <input type='submit' className='btn btn-blue' value='Add Product' />
+        </div>
+      </form>
+    </>
+  )
 }
