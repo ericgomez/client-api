e2dfa33082ac6ef68943ee729bab31b0a532c1a2
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-03-04 15:08:39 -0600

    Editing product - Show the product to edit

--- src/components/products/EditProduct.js
+++ src/components/products/EditProduct.js
@@ -3,7 +3,7 @@ import { useNavigate, useParams } from 'react-router-dom'
 import Swal from 'sweetalert2'
 
 import instanceAxios from './../../config/axios'
-import Spinner from './../layout/Spinner'
+import { Spinner } from './../../components/layout/Spinner'
 
 export const EditProduct = () => {
   /* The useParams hook returns an object of key/value pairs of the dynamic params from the 
@@ -16,15 +16,106 @@ export const EditProduct = () => {
     price: '',
     image: ''
   })
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const getProductApi = async () => {
       const response = await instanceAxios.get(`/products/${id}`)
       setProduct(response.data)
+      setLoading(false)
     }
 
     getProductApi()
-  }, [])
+  }, [id])
 
-  return <div>EditProduct</div>
+  // Method to handle the input of the form
+  const handleInput = e => {
+    setProduct({
+      ...product,
+      [e.target.name]:
+        e.target.type === 'file' ? e.target.files[0] : e.target.value
+    })
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    try {
+      const res = await instanceAxios.post('/products', product, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      })
+
+      if (res.status !== 201) {
+        Swal.fire(
+          'Product no created ',
+          'The Product is already registered ',
+          'error'
+        )
+      }
+
+      Swal.fire('Good job!', 'Your product has been created!', 'success')
+
+      // navigate to customers
+      navigate('/products', { replace: true })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // Spinner while loading
+  if (loading) return <Spinner />
+
+  const { name, price, image } = product
+
+  return (
+    <>
+      <h2>Edit Product</h2>
+
+      <form onSubmit={handleSubmit}>
+        <legend>Fill in all the fields</legend>
+
+        <div className='campo'>
+          <label>Name:</label>
+          <input
+            type='text'
+            placeholder='Product Name'
+            name='name'
+            onChange={handleInput}
+            value={name}
+          />
+        </div>
+
+        <div className='campo'>
+          <label>Price:</label>
+          <input
+            type='number'
+            name='price'
+            min='0.00'
+            step='0.01'
+            placeholder='Price'
+            onChange={handleInput}
+            value={price}
+          />
+        </div>
+
+        <div className='campo'>
+          <label>Image:</label>
+          {image && (
+            <img
+              src={`http://localhost:5000/${image}`}
+              alt={image}
+              width='300'
+            />
+          )}
+          <input type='file' name='image' onChange={handleInput} />
+        </div>
+
+        <div className='send'>
+          <input type='submit' className='btn btn-blue' value='Edit Product' />
+        </div>
+      </form>
+    </>
+  )
 }
