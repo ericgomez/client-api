ce8a207bb461aefb3efbeea3df87fbc612e48a99
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-03-04 09:50:57 -0600

    Creating new products and upload images with React

--- src/components/products/NewProduct.js
+++ src/components/products/NewProduct.js
@@ -1,6 +1,12 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Swal from 'sweetalert2'
+
+import instanceAxios from './../../config/axios'
 
 export const NewProduct = () => {
+  const navigate = useNavigate()
+
   const [product, setProduct] = useState({
     name: '',
     price: ''
@@ -22,11 +28,44 @@ export const NewProduct = () => {
     setImage(e.target.files[0])
   }
 
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    // send data to API
+    const formData = new FormData()
+    formData.append('name', product.name)
+    formData.append('price', product.price)
+    formData.append('image', image)
+
+    try {
+      const res = await instanceAxios.post('/products', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      })
+
+      if (res.status !== 201) {
+        Swal.fire(
+          'Product no created ',
+          'The Product is already registered ',
+          'error'
+        )
+      }
+
+      Swal.fire('Good job!', 'Your product has been created!', 'success')
+
+      // navigate to customers
+      navigate('/products', { replace: true })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
       <h2>New Product</h2>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <legend>Fill in all the fields</legend>
 
         <div className='campo'>
